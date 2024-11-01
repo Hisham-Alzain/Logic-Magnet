ffcf0d67146a3458f95af481afcf556965ccfd69
ThePhenomenal-Z <alzaintab@gmail.com>
2024-11-01 23:04:05 +0200

    moving magnets works

--- Board.py
+++ Board.py
@@ -5,25 +5,67 @@ class Board:
         self.no_moves = no_moves
         self.grid = [["-" for _ in range(n)] for _ in range(n)]
         for white in whites:
-            for i in range(self.n):
-                if white.pos_x == i:
-                    for j in range(self.n):
-                        if white.pos_y == j:
-                            self.grid[i][j] = "*"
+            self.grid[white.pos_x][white.pos_y] = "*"
         for magnet in magnets:
-            for i in range(self.n):
-                if magnet.pos_x == i:
-                    for j in range(self.n):
-                        if magnet.pos_y == j:
-                            if magnet.color == "red":
-                                self.grid[i][j] = "R"
-                            elif magnet.color == "purple":
-                                self.grid[i][j] = "P"
-                            elif magnet.color == "gray":
-                                self.grid[i][j] = "G"
+            if magnet.color == "red":
+                self.grid[magnet.pos_x][magnet.pos_y] = "R"
+            elif magnet.color == "purple":
+                self.grid[magnet.pos_x][magnet.pos_y] = "P"
+            elif magnet.color == "gray":
+                self.grid[magnet.pos_x][magnet.pos_y] = "G"
 
     def display_board(self):
         for i in range(self.n):
             for j in range(self.n):
                 print(self.grid[i][j] + " ", end="")
             print()
+
+    def isIngrid(self, x, y):
+        return x < self.n and y < self.n and x >= 0 and y >= 0
+
+    def move_magnet(self, x_prev, y_prev, x_next, y_next):
+        if not self.isIngrid(x_prev, y_prev) or not self.isIngrid(x_next, y_next):
+            print("out of bound")
+            return
+        elif self.grid[x_prev][y_prev] in ["-", "*"]:
+            print("wrong move")
+            return
+        elif self.grid[x_prev][y_prev] == "G":
+            print("Can't move gray magnets")
+            return
+        elif self.grid[x_prev][y_prev] == "R":
+            if self.grid[x_next][y_next] in ["-", "*"]:
+                self.grid[x_prev][y_prev] = "-"
+                self.grid[x_next][y_next] = "R"
+                for i in range(self.n):
+                    if self.grid[i][y_next] in ["G", "P"]:
+                        if self.isIngrid(i-1, y_next):
+                            if self.grid[i-1][y_next] in ["-", "*"]:
+                                self.grid[i-1][y_next] = self.grid[i][y_next]
+                                self.grid[i][y_next] = "-"
+                    if self.grid[x_next][i] in ["G", "P"]:
+                        if self.isIngrid(x_next, i-1):
+                            if self.grid[x_next][i-1] in ["-", "*"]:
+                                self.grid[x_next][i-1] = self.grid[x_next][i]
+                                self.grid[x_next][i] = "-"
+            else:
+                print("illegal move")
+        elif self.grid[x_prev][y_prev] == "P":
+            if self.grid[x_next][y_next] in ["-", "*"]:
+                self.grid[x_prev][y_prev] = "-"
+                self.grid[x_next][y_next] = "P"
+                for i in range(self.n):
+                    if self.grid[i][y_next] in ["G", "R"]:
+                        if self.isIngrid(i+1, y_next):
+                            if self.grid[i+1][y_next] in ["-", "*"]:
+                                self.grid[i+1][y_next] = self.grid[i][y_next]
+                                self.grid[i][y_next] = "-"
+                                break
+                    if self.grid[x_next][i] in ["G", "R"]:
+                        if self.isIngrid(x_next, i+1):
+                            if self.grid[x_next][i+1] in ["-", "*"]:
+                                self.grid[x_next ][i+1] = self.grid[x_next][i]
+                                self.grid[x_next][i] = "-"
+                                break
+            else:
+                print("illegal move")
--- game.py
+++ game.py
@@ -2,11 +2,13 @@ from Magnet import Magnet
 from Magnet import Node
 from Board import Board
 
-p1 = Magnet("purple",3,0)
+p1 = Magnet("red",3,0)
 g1 = Magnet("gray",2,2)
 magnets=[p1,g1]
 w11= Node(2,1)
 w12= Node(2,3)
 whites=[w11,w12]
 level1=Board(4,magnets,whites,1)
+level1.display_board()
+level1.move_magnet(3,0,0,2)
 level1.display_board()
