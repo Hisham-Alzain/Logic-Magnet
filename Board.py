6013c0fcfd6e69e5d4db281fda79786b0709dbc4
ThePhenomenal-Z <alzaintab@gmail.com>
2024-11-02 11:48:59 +0200

    game finished

--- Board.py
+++ Board.py
@@ -3,9 +3,11 @@ class Board:
     def __init__(self, n, magnets, whites, no_moves):
         self.n = n
         self.no_moves = no_moves
+        self.whites=[]
         self.grid = [["-" for _ in range(n)] for _ in range(n)]
         for white in whites:
             self.grid[white.pos_x][white.pos_y] = "*"
+            self.whites.append((white.pos_x, white.pos_y))
         for magnet in magnets:
             if magnet.color == "red":
                 self.grid[magnet.pos_x][magnet.pos_y] = "R"
@@ -19,53 +21,129 @@ class Board:
             for j in range(self.n):
                 print(self.grid[i][j] + " ", end="")
             print()
+        print()
 
     def isIngrid(self, x, y):
         return x < self.n and y < self.n and x >= 0 and y >= 0
+
+    def isGoal(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.grid[i][j] == "*":
+                    return False
+        return True
 
     def move_magnet(self, x_prev, y_prev, x_next, y_next):
         if not self.isIngrid(x_prev, y_prev) or not self.isIngrid(x_next, y_next):
             print("out of bound")
             return
+
         elif self.grid[x_prev][y_prev] in ["-", "*"]:
-            print("wrong move")
+            print("Wrong move")
             return
+
         elif self.grid[x_prev][y_prev] == "G":
             print("Can't move gray magnets")
             return
+
         elif self.grid[x_prev][y_prev] == "R":
             if self.grid[x_next][y_next] in ["-", "*"]:
-                self.grid[x_prev][y_prev] = "-"
+                if (x_prev,y_prev) in self.whites:
+                    self.grid[x_prev][y_prev] = "*"
+                else:
+                    self.grid[x_prev][y_prev] = "-"
                 self.grid[x_next][y_next] = "R"
+                self.no_moves-=1
+                magnets = []
                 for i in range(self.n):
                     if self.grid[i][y_next] in ["G", "P"]:
-                        if self.isIngrid(i-1, y_next):
-                            if self.grid[i-1][y_next] in ["-", "*"]:
-                                self.grid[i-1][y_next] = self.grid[i][y_next]
-                                self.grid[i][y_next] = "-"
+                        magnets.append((i, y_next))
                     if self.grid[x_next][i] in ["G", "P"]:
-                        if self.isIngrid(x_next, i-1):
-                            if self.grid[x_next][i-1] in ["-", "*"]:
-                                self.grid[x_next][i-1] = self.grid[x_next][i]
-                                self.grid[x_next][i] = "-"
+                        magnets.append((x_next, i))
+                self.red_moves(magnets, x_next, y_next)
             else:
                 print("illegal move")
+
         elif self.grid[x_prev][y_prev] == "P":
             if self.grid[x_next][y_next] in ["-", "*"]:
-                self.grid[x_prev][y_prev] = "-"
+                if (x_prev,y_prev) in self.whites:
+                    self.grid[x_prev][y_prev] = "*"
+                else:
+                    self.grid[x_prev][y_prev] = "-"
                 self.grid[x_next][y_next] = "P"
+                self.no_moves-=1
+                magnets = []
                 for i in range(self.n):
                     if self.grid[i][y_next] in ["G", "R"]:
-                        if self.isIngrid(i+1, y_next):
-                            if self.grid[i+1][y_next] in ["-", "*"]:
-                                self.grid[i+1][y_next] = self.grid[i][y_next]
-                                self.grid[i][y_next] = "-"
-                                break
+                        magnets.append((i, y_next))
                     if self.grid[x_next][i] in ["G", "R"]:
-                        if self.isIngrid(x_next, i+1):
-                            if self.grid[x_next][i+1] in ["-", "*"]:
-                                self.grid[x_next ][i+1] = self.grid[x_next][i]
-                                self.grid[x_next][i] = "-"
-                                break
+                        magnets.append((x_next, i))
+                self.purple_moves(magnets, x_next, y_next)
             else:
                 print("illegal move")
+
+    def purple_moves(self, magnets, x_next, y_next):
+        for magnet in magnets:
+            if magnet[0] > x_next:
+                if self.isIngrid(magnet[0] + 1, y_next):
+                    if self.grid[magnet[0] + 1][y_next] in ["-", "*"]:
+                        self.grid[magnet[0] + 1][y_next] = self.grid[magnet[0]][y_next]
+                        if (magnet[0],y_next) in self.whites:
+                            self.grid[magnet[0]][y_next] = "*"
+                        else:
+                            self.grid[magnet[0]][y_next] = "-"
+            elif magnet[0] < x_next:
+                if self.isIngrid(magnet[0] - 1, y_next):
+                    if self.grid[magnet[0] - 1][y_next] in ["-", "*"]:
+                        self.grid[magnet[0] - 1][y_next] = self.grid[magnet[0]][y_next]
+                        if (magnet[0],y_next) in self.whites:
+                            self.grid[magnet[0]][y_next] = "*"
+                        else:
+                            self.grid[magnet[0]][y_next] = "-"
+            if magnet[1] > y_next:
+                if self.isIngrid(x_next, magnet[1] + 1):
+                    if self.grid[x_next][magnet[1] + 1] in ["-", "*"]:
+                        self.grid[x_next][magnet[1] + 1] = self.grid[x_next][magnet[1]]
+                        if (x_next,magnet[1]) in self.whites:
+                            self.grid[x_next][magnet[1]] = "*"
+                        else:
+                            self.grid[x_next][magnet[1]] = "-"
+            elif magnet[1] < y_next:
+                if self.isIngrid(x_next, magnet[1] - 1):
+                    if self.grid[x_next][magnet[1] - 1] in ["-", "*"]:
+                        self.grid[x_next][magnet[1] - 1] = self.grid[x_next][magnet[1]]
+                        if (x_next,magnet[1]) in self.whites:
+                            self.grid[x_next][magnet[1]] = "*"
+                        else:
+                            self.grid[x_next][magnet[1]] = "-"
+
+    def red_moves(self, magnets, x_next, y_next):
+        for magnet in magnets:
+            if magnet[0] > x_next:
+                if self.isIngrid(magnet[0] - 1, y_next):
+                    if self.grid[magnet[0] - 1][y_next] in ["-", "*"]:
+                        self.grid[magnet[0] - 1][y_next] = self.grid[magnet[0]][
+                            y_next
+                        ]
+                        self.grid[magnet[0]][y_next] = "-"
+            elif magnet[0] < x_next:
+                if self.isIngrid(magnet[0] + 1, y_next):
+                    if self.grid[magnet[0] + 1][y_next] in ["-", "*"]:
+                        self.grid[magnet[0] + 1][y_next] = self.grid[magnet[0]][
+                            y_next
+                        ]
+                        self.grid[magnet[0]][y_next] = "-"
+            if magnet[1] > y_next:
+                if self.isIngrid(x_next, magnet[1] - 1):
+                    if self.grid[x_next][magnet[1] - 1] in ["-", "*"]:
+                        self.grid[x_next][magnet[1] - 1] = self.grid[x_next][
+                            magnet[1]
+                        ]
+                        self.grid[x_next][magnet[1]] = "-"
+            elif magnet[1] < y_next:
+                if self.isIngrid(x_next, magnet[1] + 1):
+                    if self.grid[x_next][magnet[1] + 1] in ["-", "*"]:
+                        self.grid[x_next][magnet[1] + 1] = self.grid[x_next][
+                            magnet[1]
+                        ]
+                        self.grid[x_next][magnet[1]] = "-"
--- game.py
+++ game.py
@@ -2,13 +2,281 @@ from Magnet import Magnet
 from Magnet import Node
 from Board import Board
 
-p1 = Magnet("red",3,0)
-g1 = Magnet("gray",2,2)
-magnets=[p1,g1]
-w11= Node(2,1)
-w12= Node(2,3)
-whites=[w11,w12]
-level1=Board(4,magnets,whites,1)
-level1.display_board()
-level1.move_magnet(3,0,0,2)
-level1.display_board()
+
+def play(level):
+    end_game = False
+    while not level.isGoal() and level.no_moves > 0:
+        level.display_board()
+        if input("If you want to end the game, enter 'e': ").lower() == "e":
+            print("Game ended by user.")
+            return
+        x_prev, y_prev = input(
+            "Enter the position of the magnet you want to move (x,y):"
+        ).split(",")
+        x_new, y_new = input(
+            "Enter the coordnates of where you want the to move (x,y):"
+        ).split(",")
+        level.move_magnet(int(x_prev), int(y_prev), int(x_new), int(y_new))
+    if level.isGoal():
+        level.display_board()
+        print("Congratulations! You have solved the puzzle.")
+    if level.no_moves==0:
+        level.display_board()
+        print("Game over! You ran out of moves.")
+
+
+def get_level_configuration():
+    x = int(input("Enter level (1-25): "))
+
+    levels = {
+        1: {
+            "row": 4,
+            "col": 4,
+            "purplePieces": [(2, 0)],
+            "redPieces": [],
+            "whitePieces": [(1, 1), (1, 3)],
+            "grayPieces": [(1, 2)],
+            "no_moves": 1,
+        },
+        2: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(4, 0)],
+            "redPieces": [],
+            "whitePieces": [(0, 2), (2, 0), (2, 2), (2, 4), (4, 2)],
+            "grayPieces": [(1, 2), (2, 1), (2, 3), (3, 2)],
+            "no_moves": 1,
+        },
+        3: {
+            "row": 4,
+            "col": 4,
+            "purplePieces": [(2, 0)],
+            "redPieces": [],
+            "whitePieces": [(0, 3), (2, 3)],
+            "grayPieces": [(1, 2)],
+            "no_moves": 2,
+        },
+        4: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(2, 0)],
+            "redPieces": [],
+            "whitePieces": [(0, 0), (0, 2), (4, 1)],
+            "grayPieces": [(1, 1), (3, 1)],
+            "no_moves": 2,
+        },
+        5: {
+            "row": 4,
+            "col": 4,
+            "purplePieces": [(3, 1)],
+            "redPieces": [],
+            "whitePieces": [(0, 0), (1, 0), (3, 0), (0, 2), (1, 2)],
+            "grayPieces": [(1, 0), (1, 2), (2, 0), (2, 2)],
+            "no_moves": 2,
+        },
+        6: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(2, 0)],
+            "redPieces": [],
+            "whitePieces": [(1, 2), (0, 3), (2, 3)],
+            "grayPieces": [(1, 1), (1, 3)],
+            "no_moves": 4,
+        },
+        7: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(2, 1)],
+            "redPieces": [],
+            "whitePieces": [(0, 0), (1, 0), (2, 3), (3, 2), (4, 3)],
+            "grayPieces": [(1, 0), (2, 0), (3, 1), (3, 2)],
+            "no_moves": 2,
+        },
+        8: {
+            "row": 4,
+            "col": 4,
+            "purplePieces": [(2, 0)],
+            "redPieces": [],
+            "whitePieces": [(0, 0), (0, 2), (2, 2)],
+            "grayPieces": [(1, 1), (1, 2)],
+            "no_moves": 2,
+        },
+        9: {
+            "row": 7,
+            "col": 7,
+            "purplePieces": [(0, 0)],
+            "redPieces": [],
+            "whitePieces": [(0, 1), (0, 3), (0, 6)],
+            "grayPieces": [(0, 3), (0, 5)],
+            "no_moves": 2,
+        },
+        10: {
+            "row": 4,
+            "col": 4,
+            "purplePieces": [(0, 0)],
+            "redPieces": [],
+            "whitePieces": [(1, 1), (1, 3), (3, 0), (3, 3)],
+            "grayPieces": [(2, 2), (2, 3), (3, 1)],
+            "no_moves": 2,
+        },
+        11: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [],
+            "redPieces": [(1, 2)],
+            "whitePieces": [(0, 1), (0, 2), (0, 3)],
+            "grayPieces": [(0, 0), (0, 4)],
+            "no_moves": 1,
+        },
+        12: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [],
+            "redPieces": [(3, 1)],
+            "whitePieces": [(1, 0), (2, 0), (4, 0), (4, 2)],
+            "grayPieces": [(0, 0), (1, 0), (4, 3)],
+            "no_moves": 1,
+        },
+        13: {
+            "row": 6,
+            "col": 6,
+            "purplePieces": [],
+            "redPieces": [(2, 3)],
+            "whitePieces": [(0, 3), (2, 1), (1, 1), (0, 4)],
+            "grayPieces": [(0, 0), (0, 4), (0, 5)],
+            "no_moves": 2,
+        },
+        14: {
+            "row": 4,
+            "col": 4,
+            "purplePieces": [],
+            "redPieces": [(3, 3)],
+            "whitePieces": [(1, 0), (1, 2), (2, 2), (2, 1)],
+            "grayPieces": [(0, 3), (2, 0), (3, 0)],
+            "no_moves": 2,
+        },
+        15: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(1, 2)],
+            "redPieces": [(2, 2)],
+            "whitePieces": [(1, 4), (0, 0), (0, 2), (2, 4)],
+            "grayPieces": [(0, 3), (0, 1)],
+            "no_moves": 2,
+        },
+        16: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(2, 4)],
+            "redPieces": [(2, 0)],
+            "whitePieces": [(0, 3), (0, 4), (4, 3), (4, 0)],
+            "grayPieces": [(1, 2), (3, 2)],
+            "no_moves": 3,
+        },
+        17: {
+            "row": 4,
+            "col": 4,
+            "purplePieces": [(3, 3)],
+            "redPieces": [(0, 0)],
+            "whitePieces": [(1, 1), (1, 3), (2, 2), (3, 1)],
+            "grayPieces": [(0, 2), (2, 0)],
+            "no_moves": 2,
+        },
+        18: {
+            "row": 6,
+            "col": 6,
+            "purplePieces": [(4, 3)],
+            "redPieces": [(4, 2)],
+            "whitePieces": [(2, 3), (2, 1), (2, 2), (2, 5), (1, 3)],
+            "grayPieces": [(2, 0), (0, 3), (2, 5)],
+            "no_moves": 2,
+        },
+        19: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(0, 2)],
+            "redPieces": [(2, 2)],
+            "whitePieces": [(1, 0), (3, 0), (2, 1), (3, 2), (3, 4), (1, 4)],
+            "grayPieces": [(0, 3), (0, 1), (4, 1), (4, 3)],
+            "no_moves": 4,
+        },
+        20: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(4, 2)],
+            "redPieces": [(4, 3)],
+            "whitePieces": [(0, 1), (0, 3), (1, 0), (2, 0), (3, 0)],
+            "grayPieces": [(0, 1), (0, 2), (4, 0)],
+            "no_moves": 2,
+        },
+        21: {
+            "row": 4,
+            "col": 4,
+            "purplePieces": [(2, 0)],
+            "redPieces": [(2, 3)],
+            "whitePieces": [(1, 0), (1, 1), (0, 2), (2, 0), (2, 1)],
+            "grayPieces": [(0, 1), (1, 1), (1, 2)],
+            "no_moves": 2,
+        },
+        22: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(0, 0)],
+            "redPieces": [(3, 2)],
+            "whitePieces": [(0, 1), (0, 3), (1, 0), (1, 4), (2, 1)],
+            "grayPieces": [(0, 3), (0, 4), (3, 0)],
+            "no_moves": 3,
+        },
+        23: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(3, 4)],
+            "redPieces": [(3, 2)],
+            "whitePieces": [(0, 2), (2, 1), (2, 2), (2, 3), (3, 2)],
+            "grayPieces": [(0, 3), (1, 4), (2, 0)],
+            "no_moves": 3,
+        },
+        24: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(1, 4)],
+            "redPieces": [(3, 0)],
+            "whitePieces": [(0, 3), (2, 1), (2, 3), (4, 1), (4, 2)],
+            "grayPieces": [(0, 1), (1, 3), (3, 4)],
+            "no_moves": 3,
+        },
+        25: {
+            "row": 5,
+            "col": 5,
+            "purplePieces": [(4, 0)],
+            "redPieces": [(0, 3)],
+            "whitePieces": [(0, 0), (0, 3), (2, 0), (4, 0), (4, 1), (4, 2)],
+            "grayPieces": [(0, 0), (1, 2), (3, 2), (4, 3)],
+            "no_moves": 3,
+        },
+    }
+
+    return levels.get(x, "Invalid level. Please enter a level between 1 and 25.")
+
+
+def get_level():
+    level_config = get_level_configuration()
+    magnets = []
+    for Pmagnet in level_config["purplePieces"]:
+        magnets.append(Magnet("purple", Pmagnet[0], Pmagnet[1]))
+
+    for Rmagnet in level_config["redPieces"]:
+        magnets.append(Magnet("red", Rmagnet[0], Rmagnet[1]))
+
+    for Gmagnet in level_config["grayPieces"]:
+        magnets.append(Magnet("gray", Gmagnet[0], Gmagnet[1]))
+
+    whites = [Node(w[0], w[1]) for w in level_config["whitePieces"]]
+
+    # Initialize the board
+    level = Board(level_config["row"], magnets, whites, level_config["no_moves"])
+    return level
+
+
+level = get_level()
+play(level)
